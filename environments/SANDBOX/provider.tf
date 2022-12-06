2c9609ccd0e58ba6bc419fa373ea96ebe986424a
Saidabbos <saidabbos77@gmail.com>
2022-12-05 19:54:45 -0500

    helllloooo

--- environments/SANDBOX/provider.tf
+++ environments/SANDBOX/provider.tf
@@ -9,7 +9,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "rady-bucket-1-0-0-0"
+    bucket = "aws-cicd-pipeline12321234"
     key    = "dags/myfile"
     region = "us-east-1"
   }
